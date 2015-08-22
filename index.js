a015067a03d158b9c7e6a31bf94855f4ad5f0065
Samar Panda <samar.panda84@gmail.com>
2015-08-22 21:25:21 +0530

    Fetching data from application server falcor model

--- index.js
+++ index.js
@@ -3,7 +3,42 @@ var app = express();
 var falcor = require('falcor');
 var falcorExpress = require('falcor-express');
 
+// Handy factory methods
+var $ref = falcor.Model.ref;
+var $atom = falcor.Model.atom;
+var $error = falcor.Model.error;
+
+var model = new falcor.Model({
+	cache: {
+		titlesById: {
+			234: {
+				name: "House of Cards",
+				rating: $atom(5),
+				bookmark: $error("Something went wrong")
+			}
+		},
+		genreList: [
+			{
+				name: "Recently Watched",
+				titles: [
+					$ref("titlesById[234]")
+				]
+			},
+			{
+				name: "New Releases",
+				titles: [
+					$ref("titlesById[234]")
+				]
+			}
+		]
+	}
+});
+
 app.use(express.static("."));
+app.use('/model.json', falcorExpress.dataSourceRoute(function(req, res){
+	// data source
+	return model.asDataSource();
+}));
 app.listen(9090);
 
 console.log("Navigate to http://localhost:9090");
